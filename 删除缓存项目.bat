27977e10143790fdeb149b2525131a8a6307224a
Quasar <lelouchcctony@163.com>
2016-10-15 18:20:03 +0800

    2016-10-15 delete batch

--- 删除缓存项目.bat
+++ 删除缓存项目.bat
@@ -1,3 +1,5 @@
 del /S Project\Runtime\Cache\Manager\*.php
+del /S Project\Runtime\Cache\Mobile\*.php
 del /S Project\Runtime\Logs\Common\*.log
 del /S Project\Runtime\Logs\Manager\*.log
+del /S Project\Runtime\Logs\Mobile\*.log
